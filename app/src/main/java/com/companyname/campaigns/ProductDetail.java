a59cab728c278830ef6907fd427c648788c87477
Muharrem Didici <muharrem.didici@hotmail.com>
2018-03-21 15:31:11 +0300

    Rating Method

--- app/src/main/java/com/companyname/campaigns/ProductDetail.java
+++ app/src/main/java/com/companyname/campaigns/ProductDetail.java
@@ -1,8 +1,22 @@
 package com.companyname.campaigns;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
@@ -10,13 +24,19 @@ import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
 
 import java.util.HashMap;
 
 public class ProductDetail extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
+    RatingBar ratingBar;
+
+
 
     static JSONObject proDt = null;
     private SliderLayout mDemoSlider;
@@ -26,6 +46,7 @@ public class ProductDetail extends AppCompatActivity implements BaseSliderView.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
         mDemoSlider = findViewById(R.id.slider);
+        ratingBar =  findViewById(R.id.ratingBar);
 
         // image control
         HashMap<String,String> url_maps = new HashMap<String, String>();
@@ -69,6 +90,8 @@ public class ProductDetail extends AppCompatActivity implements BaseSliderView.O
         }catch (Exception ex) {
 
         }
+        String url="http://jsonbulut.com/json/likeManagement.php";
+        raiting(ProductDetail.this,ratingBar,5,606,url);
     }
 
     @Override
@@ -97,4 +120,83 @@ public class ProductDetail extends AppCompatActivity implements BaseSliderView.O
     public void onPageScrollStateChanged(int state) {
 
     }
+
+
+
+    // json raiting  data export edit:Muharrem
+    class jsonDataRaiting extends AsyncTask<Void, Void, Void> {
+
+        String url = "";
+        HashMap<String,String> hm = new HashMap<>();
+        Context cnx = null;
+        String jsonString = "";
+        // ProgressDialog pro;
+        public jsonDataRaiting(Context cnx,String url,HashMap<String,String> hm){
+
+            this.url = url;
+            this.hm = hm;
+            this.cnx=cnx;
+            //pro = new ProgressDialog(cnx);
+            // pro.setMessage("Yükleniyor Lütfen Bekleyiniz..");
+            // pro.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                jsonString = Jsoup.connect(url).data(hm).timeout(30000).ignoreContentType(true).get().body().text();
+                //  pro.hide();
+            }catch (Exception ex) {
+                // pro.hide();
+                Toast.makeText(cnx, "İşlem Başarısız Oldu", Toast.LENGTH_SHORT).show();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+            // grafiksel işlemler bu gövdede yer alır.
+            if (!jsonString.equals("")){
+                try {
+                    JSONObject jobj = new JSONObject(jsonString);
+                    boolean durum = jobj.getJSONArray("votes").getJSONObject(0).getBoolean("durum");
+                    String mesaj = jobj.getJSONArray("votes").getJSONObject(0).getString("mesaj");
+                    if(durum) {
+
+
+                    }else {
+                        Toast.makeText(cnx, mesaj, Toast.LENGTH_SHORT).show();
+                    }
+                }catch (Exception ex) {
+                    Toast.makeText(cnx, "Json Pars Hatası", Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(cnx, "Sunucu Hatası Oluştur.. ", Toast.LENGTH_SHORT).show();
+            }
+            super.onPostExecute(aVoid);
+        }
+
+    }
+    public void raiting(final Context rcnx, RatingBar ratingBar, final int cid, final int pid, final String url){
+
+        final HashMap<String,String> hm=new HashMap<>();
+        hm.put("ref","ce7f46683b56cb84131405b848678c51");
+        hm.put("productId", String.valueOf(pid));
+        hm.put("customerId",String.valueOf(cid));
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Log.d("RATING ", "onRatingChanged: "+v);
+
+                hm.put("vote",String.valueOf(v));
+                new jsonDataRaiting(rcnx,url, hm ).execute();
+
+
+            }
+        });
+
+    }
+
 }
