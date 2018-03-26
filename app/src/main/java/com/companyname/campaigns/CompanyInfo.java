6833f6d785c1ea63bad4294c50207af05596687f
mbayindirli <mustafabayindirli27@gmail.com>
2018-03-26 21:27:02 +0300

    Google Api

--- app/src/main/java/com/companyname/campaigns/CompanyInfo.java
+++ app/src/main/java/com/companyname/campaigns/CompanyInfo.java
@@ -1,57 +1,13 @@
 package com.companyname.campaigns;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+public class CompanyInfo extends AppCompatActivity {
 
-public class CompanyInfo extends AppCompatActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_info);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map1);
-        mapFragment.getMapAsync(this);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()){
-            case android.R.id.home:
-                finish();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(41.021161,29.004065); //Kız Kulesininin kordinatları
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,13.0f));
-        mMap.addMarker(new MarkerOptions().position(sydney).title("KIZ KULESİ Adres : İstanbul Boğazı Tlf: (0555)123 45 67"));
-    }
-
 }
