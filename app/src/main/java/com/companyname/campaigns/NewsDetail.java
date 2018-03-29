518f31ca981f9c5db5190ba622dee6e91b3a9070
Hüssam Durak <hussamdurak@gmail.com>
2018-03-29 17:40:51 +0300

    NewsDetail Update

--- app/src/main/java/com/companyname/campaigns/NewsDetail.java
+++ app/src/main/java/com/companyname/campaigns/NewsDetail.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,8 +16,8 @@ import org.json.JSONObject;
 
 public class NewsDetail extends AppCompatActivity {
 
-    static JSONObject newsDt=null;
-    TextView txtnewsdetailtitle,txtnewscontent,txtnewsdescription,txtnewsdate;
+    static JSONObject newsDt = null;
+    WebView newsWebView;
     ImageView newsdetailimage;
 
     @Override
@@ -26,26 +28,22 @@ public class NewsDetail extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        txtnewsdetailtitle=findViewById(R.id.txtnewsdetailtitle);
-        txtnewscontent=findViewById(R.id.txtnewscontent);
-        txtnewsdescription=findViewById(R.id.txtnewsdescription);
-        txtnewsdate=findViewById(R.id.txtnewsdate);
+
+        newsWebView = findViewById(R.id.newsWebView);
+        newsWebView.getSettings().setJavaScriptEnabled(true);
+
 
         try {
-            String newstitle=newsDt.getString("title");
-            String newsdescription=newsDt.getString("s_description");
-            String newscontent=newsDt.getString("l_description");
-           // String newsimage=newsDt.getString("picture");
-            String newsdate=newsDt.getString("date_time");
+            String newstitle = newsDt.getString("title");
+            String newsdescription = newsDt.getString("s_description");
+            String newscontent = newsDt.getString("l_description");
+            // String newsimage=newsDt.getString("picture");
+            String newsdate = newsDt.getString("date_time");
 
            /* Log.d("", newstitle);
             Log.d("",newsdescription);
             Log.d("",newscontent);*/
 
-            txtnewsdetailtitle.setText(newstitle);
-            txtnewsdescription.setText(newsdescription);
-            txtnewscontent.setText(newscontent);
-            txtnewsdate.setText(newsdate);
 
             // image control
             ImageView newsImage = findViewById(R.id.newsdetailimage);
@@ -53,9 +51,24 @@ public class NewsDetail extends AppCompatActivity {
             String iUrl = newsDt.getString("picture");
             //Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
             Picasso.with(this).load(iUrl).into(newsImage);
-            Log.d("hata",iUrl.toString());
+            Log.d("hata", iUrl.toString());
 
+            String htmlData =
+                    "<html>\n" +
+                            "<head>\n" +
+                            "<title>News</title>\n" +
+                            "</head>\n" +
+                            "<body>\n" +
+                            "<h3 align=\"center\"> " + newstitle + " </h3>" +
+                            "<p> " + newsdescription + " </p>" +
+                            "<p> " + newscontent + " </p>" +
+                            "<p> " + newsdate + " </p>" +
+                            "</body>\n" +
+                            "</html>\n";
 
+            getSupportActionBar().setTitle(newstitle);
+            newsWebView.setWebViewClient(new WebViewClient());
+            newsWebView.loadData(htmlData, "text/html; charset=UTF-8", null);
 
 
         } catch (JSONException e) {
@@ -63,8 +76,9 @@ public class NewsDetail extends AppCompatActivity {
         }
 
     }
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
